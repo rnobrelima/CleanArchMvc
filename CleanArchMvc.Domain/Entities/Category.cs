525cdaa11abf4c866fce0de0e2ef4e724f9b004e
Rodrigo Nobre <rnobrelima@gmail.com>
2023-08-29 22:07:16 -0300

    modelo de dominio Anemico

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public ICollection<Product>? Products { get; set; }
+        
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchMvc.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Descritpion { get; set; }
+        public decimal price { get; set; }
+        public int Stock { get; set; }
+        public string? Image { get; set; }
+        public int CategoryId { get; set; }
+        public Category? category { get; set; }
+    }
+}
